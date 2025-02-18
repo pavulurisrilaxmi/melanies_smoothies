63587a0f4938602fe509fac2b10cc3a7ae1ae39a
pavulurisrilaxmi <137006174+pavulurisrilaxmi@users.noreply.github.com>
2025-02-17 19:44:42 -0600

    Update streamlit_app.py
    33,34,35 
    
    added
    
    #convert the snowspark dataframe to panda dataframe to use the LOC function
    
    pd_df= my_dataframe.to_pandas()
    #st.dataframe(pd_df)
    #st.stop()

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,11 @@ my_dataframe = session.table("smoothies.public.fruit_options").select (col('Frui
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
+#convert the snowspark dataframe to panda dataframe to use the LOC function
+
+pd_df= my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
 
 
 ingredients_list= st.multiselect(
