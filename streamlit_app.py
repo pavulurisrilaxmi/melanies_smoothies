f2086c7f0472b096587a11163851e9a3935efa01
pavulurisrilaxmi <137006174+pavulurisrilaxmi@users.noreply.github.com>
2025-02-17 20:36:25 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,15 +44,14 @@ ingredients_list= st.multiselect(
 if ingredients_list:
     #st.write(ingredients_list)
     #st.text(ingredients_list)
-    ingredients_string = ''
+    ingredients_string=''
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ''
+        ingredients_string += fruit_chosen + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
         #st.subheader(fruit_chosen + ' Nutrition Information')
@@ -66,11 +65,11 @@ if ingredients_list:
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
     #st.write(my_insert_stmt)
     #st.stop()
-    time_to_insert = st.button('Submit order')
+    time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
 
-        st.success('Your Smoothie is ordered!',icon ="✅",)
+        st.success('Your Smoothie is ordered!',icon ="✅")
 
 
 
